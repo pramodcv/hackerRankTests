2ec8c6c22044552b3fe68d9db1a6a3bae3b62213
Pramod <pramod.cv@gmail.com>
2018-08-01 08:39:01 +0530

    Linked List and Suicide Note solution

---
+++ LinkedList.java
@@ -0,0 +1,27 @@
+/*
+Detect a cycle in a linked list. Note that the head pointer may be 'null' if the list is empty.
+
+A Node is defined as:
+    class Node {
+        int data;
+        Node next;
+    }
+*/
+
+boolean hasCycle(Node head) {
+
+        if(head == null || head.next == null || head.next.next == null){
+        return false;
+        }
+        Node currentHead = head;
+        Node nextHead= currentHead.next;
+
+        do{
+        if(nextHead.next.equals(currentHead)){
+        return true;
+        }
+        currentHead = nextHead;
+        nextHead= currentHead.next;
+        }while(nextHead.next != null);
+        return false;
+        }
---
+++ suicide.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the checkMagazine function below.
+    static void checkMagazine(String[] magazine, String[] note) {
+        List<String> magazinelist = Arrays.asList(magazine);
+        List<String> notelist = Arrays.asList(magazine);
+        List<String> comparelist = notelist;
+
+        for(int i=0; i < magazine.length;i++){
+            if(notelist.contains(magazine[i])){
+                comparelist.remove(magazine[i]);
+            }
+        }
+        if(comparelist.isEmpty()){
+            System.out.println("YES");
+        } else{
+            System.out.println("NO");
+        }
+
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] mn = scanner.nextLine().split(" ");
+
+        int m = Integer.parseInt(mn[0]);
+
+        int n = Integer.parseInt(mn[1]);
+
+        String[] magazine = new String[m];
+
+        String[] magazineItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            String magazineItem = magazineItems[i];
+            magazine[i] = magazineItem;
+        }
+
+        String[] note = new String[n];
+
+        String[] noteItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            String noteItem = noteItems[i];
+            note[i] = noteItem;
+        }
+
+        checkMagazine(magazine, note);
+
+        scanner.close();
+    }
+}
